3143806aad654e612cb0f424fe2a5a8b7fff31f9
Alex Kubacki <usernotfound@gmail.com>
2026-06-12 14:45:11 -0600

    Customized user agent string

--- aptibleapi/configuration.go
+++ aptibleapi/configuration.go
@@ -89,7 +89,7 @@ type APIConfiguration struct {
 func NewAPIConfiguration() *APIConfiguration {
 	cfg := &APIConfiguration{
 		DefaultHeader:    make(map[string]string),
-		UserAgent:        "OpenAPI-Generator/1.0.0/go",
+		UserAgent:        aptibleClientVersion(),
 		Debug:            false,
 		Servers:          ServerConfigurations{
 			{
---
+++ aptibleapi/version.go
@@ -0,0 +1,14 @@
+package aptibleapi
+
+import "runtime/debug"
+
+func aptibleClientVersion() string {
+	if info, ok := debug.ReadBuildInfo(); ok {
+		for _, dep := range info.Deps {
+			if dep.Path == "github.com/aptible/aptible-api-go" {
+				return "aptible/aptible-api-go/" + dep.Version
+			}
+		}
+	}
+	return "aptible/aptible-api-go/unknown"
+}
